8bcf2a49b60fa033049cd275eaf2b68c9832c9e4
Luverduck <hyekko92@gmail.com>
2024-03-02 02:50:12 +0900

    선택 정렬 구현

--- Chapter_10/10_01/2_Selection_Sort/SelectionSort.c
+++ Chapter_10/10_01/2_Selection_Sort/SelectionSort.c
@@ -9,26 +9,26 @@ void SelectionSort(int arr[], int n)
 {
     // 데이터 교환을 위한 임시 메모리
     int temp;
-    // 현재 루프에서 최대값의 인덱스
-    int maxIDX;
+    // 현재 루프에서 최소값의 인덱스
+    int minIDX;
     // 선택 정렬
     for (int i = 0; i < n; ++i) // n개의 요소 순회
     {
         // 교환할 데이터의 인덱스 초기화
-        maxIDX = i;
+        minIDX = i;
         // 최대값 탐색
         for (int j = i + 1; j < n; ++j)
         {
-            // 현재 최대값 인덱스의 데이터가 j번째 데이터보다 큰 경우
-            if (arr[j] < arr[maxIDX])
+            // j번째 데이터가 현재 최소값 인덱스의 데이터보다 작은 경우
+            if (arr[j] < arr[minIDX])
             {
                 // 인덱스 교체
-                maxIDX = j;
+                minIDX = j;
             };
         }
         // 데이터 교환
-        temp = arr[maxIDX];
-        arr[maxIDX] = arr[i];
+        temp = arr[minIDX];
+        arr[minIDX] = arr[i];
         arr[i] = temp;
     }
 }
